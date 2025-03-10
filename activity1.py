bed88f7642bfa367e3364cf77f08e239f1159061
Abeer163 <rehmanmughis@gmail.com>
2025-03-10 18:49:10 +0000

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,13 @@
+a = 10
+b = 12
+c = 3
+d = 9
+exp = a+b**c/d+(a-d)-c+a
+print("the value of the expression is", exp)
+
+name = "Abeer"
+age = 12
+if name== 'Abeer' and age== 13:
+    print("These values are correct")
+else:
+    print("These values are incorrect")
